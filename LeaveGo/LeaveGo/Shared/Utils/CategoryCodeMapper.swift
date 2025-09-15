c2ba61a4dfa02b798609b4b15a20b875547a83c6
kitcat <kitcat92.seo@gmail.com>
2025-09-15 13:15:48 +0900

    feat: Place 관련 모델들 추가하기

---
+++ LeaveGo/LeaveGo/Shared/Utils/CategoryCodeMapper.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCodeMapper.swift
+//  LeaveGo
+//
+//  Created by Kitcat Seo on 9/14/25.
+//
+
+import SwiftUI
+
+struct CategoryCodeMapper {
+    private static let codeToNameMap: [String: String] = [
+        "A01": "자연",
+        "A02": "인문(문화/예술/역사)",
+        "A03": "레포츠",
+        "A04": "쇼핑",
+        "A05": "음식",
+        "B02": "숙박",
+        "C01": "추천코스"
+    ]
+
+    static func name(for code: String) -> String {
+        return codeToNameMap[code] ?? "기타"
+    }
+}
