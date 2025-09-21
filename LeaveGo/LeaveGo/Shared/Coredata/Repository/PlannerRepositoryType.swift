8f01a577b836e317a7f87f5090f11f9b1ab1d308
pde0814 <dongeon8943@gmail.com>
2025-09-21 20:26:51 +0900

    feat: PlannerRepo에 구현할 기능 정의 프로토콜

---
+++ LeaveGo/LeaveGo/Shared/Coredata/Repository/PlannerRepositoryType.swift
@@ -0,0 +1,23 @@
+//
+//  PlannerRepositoryType.swift
+//  LeaveGo
+//
+//  Created by 박동언 on 9/21/25.
+//
+
+import Foundation
+import CoreData
+
+// 읽기는 View에서 @FetchRequest로
+// 모든 쓰기는 이 프로토콜 통해서
+protocol PlannerRepositoryType {
+    // Planner
+    func createPlanner(_ dto: PlannerDTO) async throws -> NSManagedObjectID
+    func updatePlanner(_ id: NSManagedObjectID, _ dto: PlannerDTO) async throws
+    func deletePlanner(_ id: NSManagedObjectID) async throws
+
+    // Place
+    func addPlace(to plannerID: NSManagedObjectID, _ dto: PlaceDTO) async throws -> NSManagedObjectID
+    func updatePlace(_ placeID: NSManagedObjectID, _ dto: PlaceDTO) async throws
+    func deletePlace(_ placeID: NSManagedObjectID) async throws
+}
