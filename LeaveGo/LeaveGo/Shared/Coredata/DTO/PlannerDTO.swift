cfd91f41fa1d5d6c58686f885781ef27f2164f56
pde0814 <dongeon8943@gmail.com>
2025-09-21 20:22:56 +0900

    feat: 여행일정, 여행장소 관련 DTO 구성

---
+++ LeaveGo/LeaveGo/Shared/Coredata/DTO/PlannerDTO.swift
@@ -0,0 +1,15 @@
+//
+//  PlannerDTO.swift
+//  LeaveGo
+//
+//  Created by 박동언 on 9/21/25.
+//
+
+import Foundation
+
+//struct PlannerDTO {
+//    var title: String?
+//    var startDate: Date
+//    var endDate: Date
+//    var thumbnail: String?
+//}
