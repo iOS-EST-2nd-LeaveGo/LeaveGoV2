c92b7082d5914e84ab9e07a2b1cb73b2cd8bcc5d
pde0814 <dongeon8943@gmail.com>
2025-09-29 12:39:35 +0900

    feat: 북마크 DTO 생성

---
+++ LeaveGo/LeaveGo/Shared/Coredata/DTO/BookmarkDTO.swift
@@ -0,0 +1,14 @@
+//
+//  BookmarkDTO.swift
+//  LeaveGo
+//
+//  Created by 박동언 on 9/25/25.
+//
+
+import Foundation
+
+struct BookmarkDTO {
+    var title: String?
+    var contentID: String
+    var thumbnail: String?
+}
