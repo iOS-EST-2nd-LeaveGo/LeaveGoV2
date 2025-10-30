adce7e89f36547f39ec283b1eb845340ebc10e4e
kitcat-s <kitcat92.seo@gmail.com>
2025-10-30 17:49:09 +0900

    feat: LocationBasedEndpoint 구현하기

---
+++ LeaveGo/LeaveGo/Shared/Network/Endpoints/LocationBasedEndpoint.swift
@@ -0,0 +1,44 @@
+//
+//  LocationBasedEndpoint.swift
+//  LeaveGo
+//
+//  Created by Kitcat Seo on 10/30/25.
+//
+
+import Foundation
+
+struct LocationBasedEndpoint: Endpoint {
+    var apiType: APIType = .tour
+    let path: String = "/locationBasedList2"
+    let method: HTTPMethod = .get
+    var parameters: [String : Any]?
+    
+    let page: Int
+    let numOfRows: Int
+    let mapX: Double
+    let mapY: Double
+    let radius: Int
+    let contentTypeId: Int?
+    let arrange: String
+    
+    init(page: Int, numOfRows: Int, mapX: Double, mapY: Double, radius: Int, contentTypeId: Int?, arrange: String) {
+        self.page = page
+        self.numOfRows = numOfRows
+        self.mapX = mapX
+        self.mapY = mapY
+        self.radius = radius
+        self.contentTypeId = contentTypeId
+        self.arrange = arrange
+        
+        var parameters = defaultParameters
+        parameters["page"] = page
+        parameters["numOfRows"] = numOfRows
+        parameters["mapX"] = mapX
+        parameters["mapY"] = mapY
+        parameters["radius"] = radius
+        parameters["contentTypeId"] = contentTypeId
+        parameters["arrange"] = arrange
+        self.parameters = parameters
+    }
+}
+
