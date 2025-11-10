8c1778b58dfc48fa07af6b5dc003f26a23b24096
kitcat-s <kitcat92.seo@gmail.com>
2025-11-10 23:05:56 +0900

    remove: ContentView에서 테스트 코드 제거하기

--- LeaveGo/LeaveGo/ContentView.swift
+++ LeaveGo/LeaveGo/ContentView.swift
@@ -35,26 +35,6 @@ struct ContentView: View {
                     Text("마이페이지")
                 }
         }
-        .task {
-            Task {
-                let repository = PlaceRepository()
-                do {
-                    if let data = try await repository.fetchPlaceList(with: KeywordBasedEndpoint(page: 1, numOfRows: 2, keyword: "광화문")) {
-                        print("▶️ 장소 리스트 가져오기")
-                        print("🔥 전체 결과 \(data.totalCount)개")
-                        print("🔥 장소 목록 :", data.placeList)
-                        print("")
-                    }
-                    
-                    if let data = try await repository.fetchPlaceDetail(with: PlaceDetailEndpoint(contentType: .cultureFacility, contentID: "130821")) {
-                        print("▶️ 장소 상세 정보 가져오기")
-                        print("🔥 장소 상세 :", data)
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-        }
     }
 }
 
