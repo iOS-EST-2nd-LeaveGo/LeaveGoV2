c516b462b69f49a97702c1d23353ee3200e6909f
kitcat-s <kitcat92.seo@gmail.com>
2025-11-10 23:05:20 +0900

    refactor: PlaceRepository 에서 에러를 삼키지 않도록 do-catch 제거하기

--- LeaveGo/LeaveGo/Shared/Network/PlaceRepository.swift
+++ LeaveGo/LeaveGo/Shared/Network/PlaceRepository.swift
@@ -21,44 +21,36 @@ final class PlaceRepository {
     /// 장소 목록을 가져오는 메서드
     /// - Parameter endpoint: API 엔드포인트 정보
     /// - Returns: 장소 목록과 총 개수를 포함한 튜플, 데이터가 없으면 nil
-    /// - Throws: 네트워크 또는 디코딩 에러
-    func fetchPlaceList(with endpoint: Endpoint) async throws -> (placeList: [PlaceDTO], totalCount: Int)? {
-        do {
-            let rawData = try await networkManager.fetch(from: endpoint)
-            
-            let data = try networkManager.decode(data: rawData, type: TourResponseRoot<PlaceDTO>.self)
-            if data.response.body.totalCount != 0 {
-                return (
-                    placeList: data.response.body.items.content,
-                    totalCount: data.response.body.totalCount
-                )
-            } else {
-                print("🔥 검색된 장소가 없음")
-            }
-        } catch {
-            print(error)
+    /// - Throws: 네트워크 에러
+    func fetchPlaceList(endpoint: Endpoint) async throws -> (placeList: [PlaceDTO], totalCount: Int)? {
+        let rawData = try await networkManager.fetch(from: endpoint)
+
+        let data = try networkManager.decode(data: rawData, type: TourResponseRoot<PlaceDTO>.self)
+        if data.response.body.totalCount != 0 {
+            return (
+                placeList: data.response.body.items.content,
+                totalCount: data.response.body.totalCount
+            )
+        } else {
+            print("🔥 검색된 장소가 없음")
+            return nil
         }
-        return nil
     }
     
     /// 특정 장소의 상세 정보를 가져오는 메서드
     /// - Parameter endpoint: 장소 상세 정보 API 엔드포인트
     /// - Returns: 장소 상세 정보 DTO, 데이터가 없으면 nil
-    /// - Throws: 네트워크 에러, 디코드 에러는
-    func fetchPlaceDetail(with endpoint: PlaceDetailEndpoint) async throws -> PlaceDetailDTO? {
-        do {
-            let rawData = try await networkManager.fetch(from: endpoint)
-            
-            let data = try networkManager.decode(data: rawData, type: TourResponseRoot<PlaceDetailDTO>.self)
-            if data.response.body.totalCount != 0,
-               let placeDetail = data.response.body.items.content.first {
-                return await placeDetail.htmlCleaned()
-            } else {
-                print("🔥 contentTypeID \(endpoint.contentType.rawValue), contentID \(endpoint.contentID)에 해당하는 장소를 찾을 수 없음")
-            }
-        } catch {
-            print(error)
+    /// - Throws: 네트워크 에러
+    func fetchPlaceDetail(endpoint: PlaceDetailEndpoint) async throws -> PlaceDetailDTO? {
+        let rawData = try await networkManager.fetch(from: endpoint)
+
+        let data = try networkManager.decode(data: rawData, type: TourResponseRoot<PlaceDetailDTO>.self)
+        if data.response.body.totalCount != 0,
+           let placeDetail = data.response.body.items.content.first {
+            return await placeDetail.htmlCleaned()
+        } else {
+            print("🔥 contentTypeID \(endpoint.contentType.rawValue), contentID \(endpoint.contentID)에 해당하는 장소를 찾을 수 없음")
+            return nil
         }
-        return nil
     }
 }
